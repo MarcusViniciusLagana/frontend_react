da4aea90dabf5b986c3700cb2907feb8aecd5106
Marcus Vinicius Laganá <marcus.lagana@gmail.com>
2020-12-10 17:13:58 -0300

    Game working
    Created other functions to check victory conditions and / or draw.
    Improved style

--- src/index.js
+++ src/index.js
@@ -25,9 +25,9 @@ function Board (props) {
     return (
         <div className="game-board">
             <div>
-                <BoardRow value={props.squares.slice(1,4)} click={(i) => props.click(1 + i)}/>
-                <BoardRow value={props.squares.slice(4,7)} click={(i) => props.click(4 + i)}/>
-                <BoardRow value={props.squares.slice(7,10)} click={(i) => props.click(7 + i)}/>
+                <BoardRow value={props.squares.slice(0,3)} click={(i) => props.click(i)}/>
+                <BoardRow value={props.squares.slice(3,6)} click={(i) => props.click(3 + i)}/>
+                <BoardRow value={props.squares.slice(6,9)} click={(i) => props.click(6 + i)}/>
             </div>
         </div>
     );
@@ -39,28 +39,70 @@ class Game extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            nextMove: '',
+            nextMove: 'X',
             squares: Array(9).fill(null)
         };
+    }
+
+    restartGame() {
+        this.setState({
+            nextMove: 'X',
+            squares: Array(9).fill(null)
+        });
+    }
+    
+    checkDrawWinner() {
+        const squares = this.state.squares;
+        let draw = true;
+        let win = false;
+        let winner = '';
+    
+        for (const [a, b, c] of this.props.lines) {
+            const line = [squares[a], squares[b], squares[c]];
+            const nX = line.filter(x => x === 'X').length;
+            const nO = line.filter(x => x === 'O').length;
+            // If a line has no 'X' or no 'O', NO DRAW
+            if (nX === 0 || nO === 0) draw = false;
+            // If a line has 3 'X' or 3 'O', Win!
+            if (nX === 3 || nO === 3) {
+                win = true;
+                winner = nX === 3 ? 'X' : 'O';
+            }
+        }
+
+        if (win) return winner;
+        if (draw) return 'Draw';
+        return null;
     }
 
     squareClick(index) {
         const squares = this.state.squares;
 
-        if (squares[index]) return;
+        if (squares[index] || this.checkDrawWinner()) return;
 
         const nextMove = this.state.nextMove === 'X' ? 'O' : 'X';
-        squares[index] = nextMove;
+        squares[index] = this.state.nextMove;
 
         this.setState({ nextMove, squares });
     }
 
     render () {
+        const winner = this.checkDrawWinner();
+
         return (
-            <div className="game">
-                <Board squares={this.state.squares} click={(i) => this.squareClick(i)}/>
-                <div className="game-info">
-                    Info
+            <div>
+                <div className="title">
+                    Jogo da Velha or Tic-tac-toe
+                </div>
+                <div className="game">
+                    <Board squares={this.state.squares} click={(i) => this.squareClick(i)}/>
+                    <div className="game-info">
+                        {!winner ? 'Next Move: ' + this.state.nextMove : 
+                        winner==='Draw' ? 'Deu Velha!' : 'Winner: ' + winner}
+                    </div>
+                </div>
+                <div className="restart">
+                    <button className="restartButton" onClick={() => this.restartGame()}>Restart Game</button>
                 </div>
             </div>
         );
@@ -68,6 +110,6 @@ class Game extends React.Component {
 }
 
 ReactDOM.render(
-    <Game/>,
+    <Game lines={[[0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 3, 6],[1, 4, 7],[2, 5, 8],[0, 4, 8],[2, 4, 6]]}/>,
     document.getElementById('root')
 );
