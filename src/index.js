c5730634e9c7e0480b3a0f56a8b7c6e4d34a060c
Marcus Vinicius Laganá <marcus.lagana@gmail.com>
2020-12-10 12:05:33 -0300

    First commit
    Node project created, index.js created and index.css created

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <div className="game">
+        <div className="game-board">
+            <div>
+                <div className="board-row">
+                    <button className="square">X</button>
+                    <button className="square"></button>
+                    <button className="square"></button>
+                </div>
+                <div className="board-row">
+                    <button className="square"></button>
+                    <button className="square">X</button>
+                    <button className="square"></button>
+                </div>
+                <div className="board-row">
+                    <button className="square"></button>
+                    <button className="square"></button>
+                    <button className="square">X</button>
+                </div>
+            </div>
+        </div>
+        <div className="game-info">
+            Info
+        </div>
+    </div>,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
